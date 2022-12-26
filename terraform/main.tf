3c3ee3c5a333fd4b16007c42b2099171c04a8409
Wolf Sashok <wolfsashok85@gmail.com>
2022-12-26 14:51:39 +0000

    Terraform configured

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,28 @@ terraform {
 provider "google" {
     # Версия провайдера
     #version = "2.0.0"
-
+    credentials = file(".terraform/mygcp-creds.json")
     # ID проекта
     project = "original-list-372101"
 
-    region = "europe-west-1"
+    region = "europe-west4-a"
+}
+
+resource "google_compute_instance" "app" {
+    name = "reddit-app"
+    machine_type = "e2-micro"
+    zone = "europe-west4-a"
+    # определение загрузочного диска
+    boot_disk {
+        initialize_params {
+        image = "debian-11-bullseye-v20221206"
+        }
+    }
+    # определение сетевого интерфейса
+    network_interface {
+        # сеть, к которой присоединить данный интерфейс
+        network = "default"
+        # использовать ephemeral IP для доступа из Интернет
+        access_config {}
+    }
 }
