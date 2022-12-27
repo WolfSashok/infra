0caf206b8adf5edef72bcc619e3a2475d360c330
Wolf Sashok <wolfsashok85@gmail.com>
2022-12-27 18:54:25 +0000

    VSC

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,12 +18,31 @@ resource "google_compute_instance" "app" {
     name = "reddit-app"
     machine_type = "e2-micro"
     zone = "europe-west4-a"
+    tags = ["reddit-app"]
     # определение загрузочного диска
     boot_disk {
         initialize_params {
         image = "debian-11-bullseye-v20221206"
         }
     }
+
+    connection {
+        type = "ssh"
+        user = "wolfsashok85"
+        agent = false
+        # путь до приватного ключа
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+
+    provisioner "file" {
+        source = "files/puma.service"
+        destination = "/tmp/puma.service"
+    }
+
+    provisioner "remote-exec" {
+        script = "files/deploy.sh"
+    }
+
     # определение сетевого интерфейса
     network_interface {
         # сеть, к которой присоединить данный интерфейс
@@ -31,4 +50,19 @@ resource "google_compute_instance" "app" {
         # использовать ephemeral IP для доступа из Интернет
         access_config {}
     }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+    name = "allow-puma-default"
+    # Название сети, в которой действует правило
+    network = "default"
+    # Какой доступ разрешить
+    allow {
+        protocol = "tcp"
+        ports = ["9292"]
+    }
+    # Каким адресам разрешаем доступ
+    source_ranges = ["0.0.0.0/0"]
+    # Правило применимо для инстансов с перечисленными тэгами
+    target_tags = ["reddit-app"]
 }
