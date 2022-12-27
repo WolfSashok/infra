c95436ed3fb3b3f2b0fa38cd3ab9a54157ea017f
= <=>
2022-12-27 21:40:43 +0200

    VSC_home_branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
     required_version = "1.3.6"
 }
 #VSC installed
-#Правка старого клона
+#Новая ветка VSC_home_branch
 provider "google" {
     # Версия провайдера
     #version = "2.0.0"
